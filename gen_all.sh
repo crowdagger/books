bfad186a2541803d62e1a4d0217a41f58af6c888
Miroslav Krajicek <mirokrajicek@gmail.com>
2018-08-13 23:54:58 +0200

    updated trpl-2018.book, trpl submodule and gen_all.sh script

--- gen_all.sh
+++ gen_all.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-# Generate EPUB, HTML and A5 version
+echo Generate EPUB, HTML and A5 version
 crowbook trpl1.book
 crowbook trpl2.book
+crowbook trpl-2018.book
 crowbook cargo.book
 
-# Generate A4 version
+echo Generate A4 version
 crowbook trpl1.book --to pdf --set tex.paper_size a4paper output.pdf trpl1-a4.pdf
 crowbook trpl2.book --to pdf --set tex.paper_size a4paper output.pdf trpl2-a4.pdf
+crowbook trpl-2018.book --to pdf --set tex.paper_size a4paper output.pdf trpl-2018-a4.pdf
 crowbook cargo.book --to pdf --set tex.paper_size a4paper output.pdf cargo-a4.pdf
 
-# Generate letter version
+echo Generate letter version
 crowbook trpl1.book --to pdf --set tex.paper_size letterpaper output.pdf trpl1-letter.pdf
 crowbook trpl2.book --to pdf --set tex.paper_size letterpaper output.pdf trpl2-letter.pdf
+crowbook trpl-2018.book --to pdf --set tex.paper_size letterpaper output.pdf trpl-2018-letter.pdf
 crowbook cargo.book --to pdf --set tex.paper_size letterpaper output.pdf cargo-letter.pdf
 
